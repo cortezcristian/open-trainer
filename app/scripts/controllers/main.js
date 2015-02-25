b8852f89d65a6e9fce3640f032ee6cacb3dd9b14
Cristian Cortez <cortez.cristian@gmail.com>
2015-02-25 14:38:33 -0300

    yo install

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openTrainerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the openTrainerApp
+ */
+angular.module('openTrainerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
